e9e89b034ad4f772f0aa6b99c43829b2ededcdc0
Александр Котов <alexandr.kotov@playrix.com>
2023-06-19 16:59:32 +0400

    Game Settings implemented

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -2,19 +2,13 @@ using Leopotam.EcsLite;
 using System;
 using TestAsssignment.Configs;
 using TestAsssignment.Systems;
-using TestAsssignment.UI;
 using UnityEngine;
 
 namespace TestAsssignment
 {
     public class Startup : MonoBehaviour
     {
-        [Serializable]
-        public class Businesess
-        {
-            public BusinessConfig[] data;
-        }
-        public Businesess businesses;
+        public GameSettingsConfig gameSettings;
 
         public class SharedData
         {
@@ -41,23 +35,21 @@ namespace TestAsssignment
         }
         private SharedData _sharedData;
 
-        public GameScreenComponent gameScreenPrefab;
-
         private EcsWorld _world;
         private IEcsSystems _systems;
 
-        void Start()
+        private void Start()
         {
-            _sharedData = new SharedData(100);
+            _sharedData = new SharedData(gameSettings.StartBalance);
 
             _world = new EcsWorld();
             _systems = new EcsSystems(_world, _sharedData);
             _systems
-                .Add(new BusinessCreateSystem(businesses.data))
+                .Add(new BusinessCreateSystem(gameSettings.BusinessConfigs))
                 .Add(new BusinessProfitSystem())
                 .Add(new AddMoneySystem())
                 .Add(new SpendMoneySystem())
-                .Add(new GameUISystem(gameScreenPrefab))
+                .Add(new GameUISystem(gameSettings.GameScreenPrefab))
 #if UNITY_EDITOR
                 .Add(new Leopotam.EcsLite.UnityEditor.EcsWorldDebugSystem())
                 .Add(new Leopotam.EcsLite.UnityEditor.EcsSystemsDebugSystem())
@@ -65,12 +57,12 @@ namespace TestAsssignment
                 .Init();
         }
 
-        void Update()
+        private void Update()
         {
             _systems?.Run();
         }
 
-        void OnDestroy()
+        private void OnDestroy()
         {
             _systems?.Destroy();
             _systems = null;
